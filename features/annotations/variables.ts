bf4d8f66832c728f306a0562a96e5d4f24737afb
nfabacus <n.fujioka@abacusmaths.info>
2020-06-14 22:11:27 +0100

    reviewing basic annotations in variables.ts file

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -9,3 +9,45 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 const now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2.5,3];
+let myBooleans: boolean[] = [ false, true, true ];
+
+// class
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number, y: number, greeting: string }= {
+  x: 10,
+  y: 20,
+  greeting: "Hello"
+}
+
+// function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number, y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialise it later
+let myBoolean: boolean;
+myBoolean = true;
+
+// 3) Variables whose type cannot be inferred correctly.
+let numbers = [ -10, -1, 12];
+let numberAboveZero: boolean | number = false;  // type can be boolean or number
+
+for (let i = 0; i < numbers.length; i++) {
+  numberAboveZero = numbers[i];
+}
